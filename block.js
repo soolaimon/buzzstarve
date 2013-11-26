485cd2bba584f83b7fc4f5699c03078e71758644
Dave Nelson <davidicus21@gmail.com>
2013-11-25 22:55:30 -0700

    First commit

---
+++ block.js
@@ -0,0 +1,13 @@
+$document.ready(function() {
+
+$('uiUnifiedstory').remove();
+	console.log('hi');
+});
+
+// var deletePost = function(post){
+
+// 	post.remove();
+
+// }
+
+// $document.ready( deletePost($post) );
